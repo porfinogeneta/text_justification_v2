0467002db769b2638f422982e700344bb06f345c
porfinogeneta <szyman123drum@gmail.com>
2023-01-24 13:06:36 +0100

    prepared for ident introduction

--- headers.h
+++ headers.h
@@ -6,6 +6,8 @@ void prepare_text(wchar_t text[], int N, int *finishedPreparing, wchar_t *buffer
 void justify_center(wchar_t buffer[], wchar_t input[], int N, int alignment, int ident);
 void givingResult(wchar_t buffer[], int N, int even, int ident, int alignment);
 void blowSpaces(wchar_t result[], wchar_t buffer[], int words_counter, int ident, int even, int is_last_row, int alignment, int N, int holes, int spaces_per_hole, int additional_spaces, int letters_counter);
+void lastRowAlignment(wchar_t buffer[], wchar_t result[], int spaces_available, int spaces_per_hole, int additional_spaces, int is_last_row, int alignment);
+void additionalSpacesDistribution(wchar_t result[], int even, int additional_spaces, int words_counter, int N);
 
 
 
